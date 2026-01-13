e8bb3c9823f161d21f4d6cbafd6e8fbdd9b01399
delefatobs <delefatobs@outlook.com>
2026-01-13 17:57:29 +0000

    saving your changes

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm 
+import click
 
 dtype = {
     "VendorID": "Int64",
@@ -40,6 +41,23 @@ def run():
     month = 1
     target_table = 'yellow_taxi_data'
     chunksize = 100000
+    
+    
+@click.command()
+@click.option('--pg_user', default='root', help='PostgreSQL username')
+@click.option('--pg_pass', default='root', help='PostgreSQL password')
+@click.option('--pg_host', default='localhost', help='PostgreSQL host')
+@click.option('--pg_port', default=5432, help='PostgreSQL port')
+@click.option('--pg_db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--target_table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
+
+    """ Ingestion NYC taxi data into PostgreSQL database. """
+
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
@@ -69,7 +87,6 @@ def run():
         con=engine,
         if_exists='append'
     )
-
 
 
 if __name__ == '__main__':
